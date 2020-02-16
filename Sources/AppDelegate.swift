b724dcfe383594457eb4cc4e4d112d45b78a33f6
Adriano Soares <gsoares.adriano@gmail.com>
2020-02-15 23:05:48 -0300

    change format options

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -9,12 +9,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-
         let nav = UINavigationController()
 
         coordinator = AppCoordinator(navigationController: nav)
         coordinator?.start()
-        
+
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
